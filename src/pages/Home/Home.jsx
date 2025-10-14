64b480239a5c6342a48c0ca13cffc4a017cbb09d
Matheus Sixel <msixel22@gmail.com>
2025-10-14 14:39:00 -0300

    iniciando estilização page Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <div className='home'>
       <div className="hero">
-        <h1>Largest <br /> Crypto Marketplace</h1>
+        <h1>Largest<br /> Crypto Marketplace</h1>
         <p>
           Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos
+        </p>
           <form>
             <input type="text" placeholder='Search crypto...' />
             <button type='submit'>Search</button>
           </form>
-        </p>
       </div>
     </div>
   )
