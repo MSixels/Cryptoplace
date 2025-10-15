2a128a823ba11096e468c9531cf3c683bec43e2d
Matheus Sixel <msixel22@gmail.com>
2025-10-15 10:34:16 -0300

    Criando componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2025, Cryptoplace - Todos os direitos reservados.</p>
+    </div>
+  )
+}
+
+export default Footer
